b5348fda99b1ae3fb056dd53f23e96d29e9c335b
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-09-17 23:07:26 +0700

    ENHANCEMENT: Add extension to site config

--- _config.php
+++ _config.php
@@ -1,4 +1,5 @@
 <?php 
 Object::add_extension('SiteTree', 'AccessKeyExtension');
-Object::add_extension("Page_Controller","AccessKeyExtension");
+Object::add_extension("Page_Controller","AccessKeysControllerExtension");
+DataObject::add_extension('SiteConfig', 'AccessKeysSiteConfig');
  ?>
